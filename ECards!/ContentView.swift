4f17b4275089ffb7125c8106d7bcca47093ffbbb
Dmitry Balantaev <dmitrybalantaev@gmail.com>
2022-02-03 11:17:34 +0600

    Initial Commit

---
+++ ECards!/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ECards!
+//
+//  Created by Дмитрий Балантаев on 03.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ECards!/ECards_App.swift
@@ -0,0 +1,17 @@
+//
+//  ECards_App.swift
+//  ECards!
+//
+//  Created by Дмитрий Балантаев on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ECards_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
